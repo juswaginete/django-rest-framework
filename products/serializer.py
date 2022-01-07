7ddc3a5239fe29897691d22dcadb61aae350d3c5
James Joshua Ginete <james.ginete10@gmail.com>
2022-01-07 16:46:00 +0800

    Add fix for creating product object

--- products/serializer.py
+++ products/serializer.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User, Group
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
 
 from .models import Products, ProductsType
@@ -17,21 +18,52 @@ class ProductsSerializer(serializers.ModelSerializer):
         name = self.data.get('name')
         quantity = self.data.get('quantity')
         price = self.data.get('price')
+        product_type_id = self.data.get('product_type')
 
-        product = Products(
-            name=name,
-            quantity=quantity,
-            price=price
-        )
+        product_type = ProductsType.objects.get(id=product_type_id)
 
-        product.save()
+        # try:
+        #     product = Products(
+        #         name=name,
+        #         quantity=quantity,
+        #         price=price,
+        #         product_type=product_type
+        #     )
 
-        return {
-            "id": product.id,
-            "name": product.name,
-            "quantity": product.quantity,
-            "price": product.price
-        }
+        #     product.save()
+
+        #     return {
+        #         "id": product.id,
+        #         "name": product.name,
+        #         "quantity": product.quantity,
+        #         "price": product.price,
+        #         "product_type": product.product_type.id
+        #     }
+        # except Exception as e:
+        #     raise e
+
+        if product_type:
+            product = Products(
+                name=name,
+                quantity=quantity,
+                price=price,
+                product_type=product_type
+            )
+
+            product.save()
+
+            return {
+                "id": product.id,
+                "name": product.name,
+                "quantity": product.quantity,
+                "price": product.price,
+                "product_type": {
+                    "id": product.product_type.id,
+                    "type_name": product.product_type.type_name,
+                    "date_created": product.product_type.date_created,
+                    "date_updated": product.product_type.date_updated,
+                }
+            }
 
 
 
@@ -58,7 +90,7 @@ class ProductsTypeSerializer(serializers.ModelSerializer):
         productType.save()
 
         return {
-            "type name": productType.type_name,
-            "date created": productType.date_created,
-            "date updated": productType.date_updated
+            "type_name": productType.type_name,
+            "date_created": productType.date_created,
+            "date_updated": productType.date_updated
         }
